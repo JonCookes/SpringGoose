e07cbb5377b4d18368dd0f69480a008af151d7df
FIRST_NAME LAST_NAME <My_NAME@example,com>
2022-02-03 14:03:45 +0000

    Played around with the controller

--- src/main/java/com/qa/SpringGoose/controller/GooseController.java
+++ src/main/java/com/qa/SpringGoose/controller/GooseController.java
@@ -1,5 +1,67 @@
 package com.qa.SpringGoose.controller;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.SpringGoose.domain.Goose;
+import com.qa.SpringGoose.service.ServiceInterface;
+
+@RestController
+@RequestMapping("/Goose")
 public class GooseController {
+
+	private ServiceInterface<Goose> service;
+
+	public GooseController(ServiceInterface service) {
+		this.service = service;
+	}
+
+	// Create
+	@PostMapping("/create")
+	public ResponseEntity<Goose> createCadie(@RequestBody Goose goose) {
+		return new ResponseEntity<Goose>(this.service.create(goose), HttpStatus.CREATED);
+	}
+
+	@PostMapping("/create")
+	public ResponseEntity<Goose> createGoose(@RequestBody Goose goose) {
+		return new ResponseEntity<Goose>(this.service.create(goose), HttpStatus.CREATED);
+	}
+
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Goose>> readAllGoose() {
+		return new ResponseEntity<List<Goose>>(this.service.readAll(), HttpStatus.FOUND);
+	}
+
+	@GetMapping("/getById/{id}")
+	public ResponseEntity<Goose> readByIdGoose(@PathVariable Long id) {
+		return new ResponseEntity<Goose>(this.service.readById(id), HttpStatus.FOUND);
+	}
+
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Goose> updateGoose(@PathVariable Long id, @RequestBody Goose goose) {
+		return new ResponseEntity<Goose>(this.service.update(id, goose), HttpStatus.ACCEPTED);
+
+	}
+
+	@Transactional
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<Boolean> deleteGoose(@PathVariable Long id) {
+		boolean deleted = this.service.delete(id);
+
+		return (deleted) ? new ResponseEntity<Boolean>(deleted, HttpStatus.OK)
+				: new ResponseEntity<Boolean>(deleted, HttpStatus.NOT_FOUND);
+	}
 
 }
--- src/main/java/com/qa/SpringGoose/service/ServiceInterface.java
+++ src/main/java/com/qa/SpringGoose/service/ServiceInterface.java
@@ -1,5 +1,25 @@
 package com.qa.SpringGoose.service;
 
-public interface ServiceInterface {
+import java.util.List;
+
+public interface ServiceInterface<T> {
+	
+
+	
+	//Create a create method 
+	
+	T create(T t);
+	
+	// READ All
+	List<T> readAll();
+
+	//Read By Id 
+	T readById(Long id);
+	
+	//Update // We take the id to identify where it is, we take in an object to then replace its contents.
+	T update(Long id, T t);
+	
+	//Delete
+	boolean delete(Long id);
 
 }
